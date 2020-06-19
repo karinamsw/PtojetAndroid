7a330ddcbfc7f6e5940b757277de65a2cc17d682
karina <moussaoui.karina@gmail.com>
2020-06-20 00:40:56 +0200

    Add MVC architecture for MainScreen

--- app/src/main/java/com/example/myelephant/presentation/view/MainActivity.java
+++ app/src/main/java/com/example/myelephant/presentation/view/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import com.example.myelephant.Constants;
 import com.example.myelephant.R;
 import com.example.myelephant.data.ElephantApi;
+import com.example.myelephant.presentation.controller.MainController;
 import com.example.myelephant.presentation.model.Elephant;
 import com.example.myelephant.presentation.model.RestElephantResponse;
 import com.google.gson.Gson;
@@ -31,44 +32,28 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String BASE_URL = "https://raw.githubusercontent.com/karinamsw/PtojetAndroid/master/";
-
-
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private SharedPreferences sharedPreferences;
-    private Gson gson;
+
+    private MainController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sharedPreferences = getSharedPreferences("application_elephant", Context.MODE_PRIVATE);
-        gson = new GsonBuilder().setLenient().create();
+        controller = new MainController(
+               this,
+                new GsonBuilder().setLenient().create(),
+                getSharedPreferences("application_elephant", Context.MODE_PRIVATE)
 
-        List<Elephant> elephantList = getDataFromCache();
+        );
+        controller.onStart();
 
-        if(elephantList != null ){
-            showList(elephantList);
-        } else {
-            makeApiCall();
-        }
     }
 
-    private List<Elephant> getDataFromCache(){
-        String jsonElephant = sharedPreferences.getString(Constants.KEY_ELEPHANT_LIST, null);
-
-        if(jsonElephant == null){
-            return null;
-        } else {
-           Type listType = new TypeToken<List<Elephant>>(){}.getType();
-           return gson.fromJson(jsonElephant, listType);
-        }
-    }
-
-    private void showList (List <Elephant> elephantList) {
+    public void showList (List <Elephant> elephantList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
          recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
@@ -79,52 +64,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        private void makeApiCall (){
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
 
-            ElephantApi elephantApi = retrofit.create(ElephantApi.class);
-
-            Log.d("VINCE", "BEFORE CALLBACK");
-            Call<RestElephantResponse> call = elephantApi.getElephantResponse();
-            call.enqueue(new Callback<RestElephantResponse>() {
-
-
-                @Override
-                public void onResponse(Call<RestElephantResponse> call, Response<RestElephantResponse> response) {
-                    Log.d("VINCE", "INSIDE CALLBACK");
-
-                    if (response.isSuccessful() && response.body() != null) {
-                        List<Elephant> elephantList = response.body().getResults();
-                        saveList(elephantList);
-                        showList(elephantList);
-                        Toast.makeText(getApplicationContext(), "API Success", Toast.LENGTH_SHORT).show();
-
-                    } else {
-                        showError();
-                    }
-                }
-
-                @Override
-                public void onFailure(@NonNull Call<RestElephantResponse> call, Throwable t) {
-                    showError();
-                }
-            });
-        }
-
-    private void saveList(List<Elephant> elephantList) {
-       String jsonString = gson.toJson(elephantList);
-        sharedPreferences
-                    .edit()
-                    .putString(Constants.KEY_ELEPHANT_LIST,jsonString)
-                    .apply();
-
-
-    }
-
-    private void showError (){
+    public void showError (){
             Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
 
         }
